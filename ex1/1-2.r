f1f1984bbd2b064e8277316b99ab7ca15042f006
Unknown <lorenz.graaf@gmail.com>
2018-11-14 17:05:49 +0100

    2.1-2.2 progress

--- ex1/1-2.r
+++ ex1/1-2.r
@@ -1,4 +1,25 @@
 library(ggplot2)
+library(lattice) # non-standard plotting library
 data(diamonds)
 
+# 2.1
+# samples and features
+samples <- dim(diamonds)[1]
+features <- dim(diamonds)[2]
 
+# data types
+str(diamonds)
+
+# balance
+plot(diamonds$color) 
+plot(diamonds$cut)
+plot(diamonds$clarity)
+
+# 2.2
+hist(diamonds$price) 
+boxplot(diamonds$price)  
+densityplot(diamonds$price, plot.points = F)
+
+# Is there a Trend? yes:
+# What Trend? Exponential shrinking.
+# From which plots can you derive it? The trend can be derived best from the density (and hist) plot.
